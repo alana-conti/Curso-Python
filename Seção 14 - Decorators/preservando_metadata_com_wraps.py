78d5bb72035ff67f5b1a812a32dd41766ab6f51b
alana-conti <alana.v.conti@gmail.com>
2025-07-17 23:18:32 +0000

    Desenvolvida a seção 14

---
+++ Seção 14 - Decorators/preservando_metadata_com_wraps.py
@@ -0,0 +1,75 @@
+"""
+Preservando Metadata com Wraps
+
+Metadados -> São dados intrínsecos em arquivos.
+
+Wraps -> São funções que envolvem elementos com diversas finalidades.
+
+# Problema
+
+def ver_log(funcao):
+    def logar(*args, **kwargs):
+        ""Eu sou uma função (logar) dentro de outra""
+        print(f'Você está chamando {funcao.__name__}')
+        print(f'Aqui a documentação: {funcao.__doc__}')
+        return funcao(*args, **kwargs)
+    return logar
+
+@ver_log
+def soma(a, b):
+    ""Soma dois números.""
+    return a + b
+
+# print(soma(10, 30))
+
+print(soma.__name__) # Soma
+print(soma.__doc__) # Soma dois números.
+
+# Resolução do Problema
+from functools import wraps
+
+def ver_log(funcao):
+    @wraps(funcao)
+    def logar(*args, **kwargs):
+        ""Eu sou uma função (logar) dentro de outra""
+        print(f'Você está chamando {funcao.__name__}')
+        print(f'Aqui a documentação: {funcao.__doc__}')
+        return funcao(*args, **kwargs)
+    return logar
+
+@ver_log
+def soma(a, b):
+    ""Soma dois números.""
+    return a + b
+
+# print(soma(10, 30))
+
+print(soma.__name__) # Soma
+print(soma.__doc__) # Soma dois números
+
+print(help(soma))
+"""
+
+# Resolução do Problema
+from functools import wraps
+
+def ver_log(funcao):
+    @wraps(funcao)
+    def logar(*args, **kwargs):
+        """Eu sou uma função (logar) dentro de outra""" 
+        print(f'Você está chamando {funcao.__name__}')
+        print(f'Aqui a documentação: {funcao.__doc__}')
+        return funcao(*args, **kwargs)
+    return logar
+
+@ver_log
+def soma(a, b):
+    """Soma dois números."""
+    return a + b
+
+# print(soma(10, 30))
+
+print(soma.__name__) # Soma
+print(soma.__doc__) # Soma dois números.
+
+print(help(soma))
