9857684de032872175b3f2dcb988fc1084f39516
alana-conti <alana.v.conti@gmail.com>
2025-07-22 19:35:23 +0000

    Desenvolvida a seção 20

---
+++ Seção 20 - Gerenciamento de Memória em Python/single_thread.py
@@ -0,0 +1,17 @@
+import time
+from threading import Thread
+
+CONTADOR = 50000000
+
+def contagem_regressiva(n):
+    while n > 0:
+        n -= 1
+
+
+inicio = time.time()
+contagem_regressiva(CONTADOR)
+fim = time.time()
+
+print(f'Tempo em segundos - {fim - inicio}')
+
+# Tempo em segundos - 1.4936716556549072
